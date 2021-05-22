bec0aa8defc0bc0a45732d112a9ea6d7fef618c3
ashu.jain <v-ashu.jain@hotwire.com>
2021-05-22 21:20:00 +0530

    styled homepage menu-items and added shop page components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+
+
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      
+      <Route exact path='/' component={HomePage} />
+      <Route   path='/shop' component={ShopPage} />
+      
     </div>
   );
 }
