698ae518912dc084c55891dd77e2bb7d49c90419
ashu.jain <v-ashu.jain@hotwire.com>
2021-06-12 12:57:01 +0530

    added new data to shop_data.js and added shop.utils.js to group data by id

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -5,6 +5,14 @@ const toggleCartHidden = () => {
     return {
         type:CartActionTypes.TOGGLE_CART_HIDDEN,
 
+
+    }
+}
+
+export const addItem = (item) => {
+    return {
+        type:CartActionTypes.ADD_ITEM,
+        payload:item
     }
 }
 
